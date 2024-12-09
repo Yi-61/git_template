018963aa42a38d319e4116b1ef2a64cadbb6c990
Yi Liu <yiliu021@stanford.edu>
2024-12-09 15:08:00 -0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='ngcd',
+      version='0.1',
+      description='Near go cue dynamics project.',
+      author='Yi Liu',
+      author_email='yiliu021@stanford.edu',
+      packages=find_packages()
+     )
